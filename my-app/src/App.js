947184b2adbb5b81986b3f24a3737a144bcd7902
dannikaml <dannikaml@hotmail.com>
2023-05-04 10:37:19 -0500

    fixed app.css/js for header

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -4,26 +4,19 @@ import './App.css';
 
 function App() {
       return (
+        <div className="App">
         <header className="App-Header">
           <h1>Dannika Long</h1>
           <nav>
             <ul>
-              <li>
-                <a href="#about">About</a>
-              </li>
-              <li>
-                <a href="#projects">Projects</a>
-              </li>
-              <li>
-                <a href="#contact">Contact</a>
-              </li>
-              <li>
-                <a href="#resume">Resume</a>
-              </li>
+                <a href="#about"> About  </a>
+                <a href="#projects"> Projects  </a>
+                <a href="#contact"> Contact  </a>
+                <a href="#resume"> Resume  </a>
             </ul>
           </nav>
     </header>
-    
+    </div>
       )
     };
 
