bd118c1d3f567f749ed0165b159c22861f95f7f5
dannikaml <dannikaml@hotmail.com>
2023-05-04 10:22:56 -0500

    added header

---
+++ my-app/src/App.js
@@ -0,0 +1,31 @@
+// import logo from './logo.svg';
+import './App.css';
+// import  './components/Header'; 
+
+function App() {
+      return (
+        <header className="App-Header">
+          <h1>Dannika Long</h1>
+          <nav>
+            <ul>
+              <li>
+                <a href="#about">About</a>
+              </li>
+              <li>
+                <a href="#projects">Projects</a>
+              </li>
+              <li>
+                <a href="#contact">Contact</a>
+              </li>
+              <li>
+                <a href="#resume">Resume</a>
+              </li>
+            </ul>
+          </nav>
+    </header>
+    
+      )
+    };
+
+
+export default App;
